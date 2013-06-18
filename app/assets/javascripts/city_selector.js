ff0d5c9a9a101b8a2fee2b933904f44cb98fb4fb
W!ze Flux <fear.ru@gmail.com>
2013-06-18 13:18:36 +0800

    new query form

--- app/assets/javascripts/city_selector.js
+++ app/assets/javascripts/city_selector.js
@@ -1,53 +1,68 @@
 CitySelector = function($scope) {
+  $scope.comboBoxOpen = false;
   $scope.loadedCities = [];
-  $scope.preloadedCity = [];
-  $scope.loading = false;
-
-  $scope.allCities = [{
-    title: 'Все города',
-    cid: 'all',
-    selected: false
-  }];
-
-  $scope.preloadCity = function(cid) {
-    if (cid == 'all') {
-      $scope.allCities[0].selected = true
-    } else {
-      VK.api("places.getCityById", {cids: cid}, function(data) {
-        $scope.$apply(function() {
-          $scope.preloadedCity = [{
-            title: data.response[0].name, cid: data.response[0].cid, selected: true
-          }];
-        });
-      });      
-    };
-  };
-
+  $scope.selectedCity = {};
+  
   $scope.$watch('search', function(newValue, oldValue) {  $scope.loadCities()  });
-
-  $scope.cities = function() {
-    return $scope.allCities.concat($scope.preloadedCity, $scope.loadedCities)
+  
+  $scope.select = function(city) {
+    $scope.selectedCity = city;
+    $scope.toggleComboBox();
   };
-
+  
+  $scope.toggleComboBox = function() {  $scope.comboBoxOpen = !$scope.comboBoxOpen  };
+  
   $scope.loadCities = function() {
     VK.api("places.getCities", {country: 1, q: $scope.search}, function(data) {
       $scope.$apply(function() {  $scope.loadedCities = data.response  });
     });
   };
-
-  $scope.selectedCity = function() {
-    return _.detect($scope.cities(), function(c) {  return c.selected == true  });
-  };
-
-  $scope.displaySearch = function() {
-    return (typeof $scope.selectedCity() == 'undefined');
-  };
-
-  $scope.hide = function(city) {
-    if ($scope.selectedCity()) {  return city != $scope.selectedCity()  } else {  return false  };
-  };
   
-  $scope.select = function(city) {
-    if (city.selected) {  city.selected = false  } else {  city.selected = true  };
-  };
+  // $scope.loadedCities = [];
+  // $scope.preloadedCity = [];
+  // $scope.loading = false;
+  // 
+  // $scope.allCities = [{
+  //   title: 'Все города',
+  //   cid: 'all',
+  //   selected: false
+  // }];
+  // 
+  // $scope.preloadCity = function(cid) {
+  //   if (cid == 'all') {
+  //     $scope.allCities[0].selected = true
+  //   } else {
+  //     VK.api("places.getCityById", {cids: cid}, function(data) {
+  //       $scope.$apply(function() {
+  //         $scope.preloadedCity = [{
+  //           title: data.response[0].name, cid: data.response[0].cid, selected: true
+  //         }];
+  //       });
+  //     });      
+  //   };
+  // };
+  // 
+  // 
+  // 
+  // $scope.cities = function() {
+  //   return $scope.allCities.concat($scope.preloadedCity, $scope.loadedCities)
+  // };
+  // 
+
+  // 
+  // $scope.selectedCity = function() {
+  //   return _.detect($scope.cities(), function(c) {  return c.selected == true  });
+  // };
+  // 
+  // $scope.displaySearch = function() {
+  //   return (typeof $scope.selectedCity() == 'undefined');
+  // };
+  // 
+  // $scope.hide = function(city) {
+  //   if ($scope.selectedCity()) {  return city != $scope.selectedCity()  } else {  return false  };
+  // };
+  // 
+  // $scope.select = function(city) {
+  //   if (city.selected) {  city.selected = false  } else {  city.selected = true  };
+  // };
 };
--- app/assets/javascripts/tags_selector.js
+++ app/assets/javascripts/tags_selector.js
@@ -1,7 +1,23 @@
 TagsSelector = function($scope) {
   $scope.tags = [];
+  $scope.comboBoxOpen = false;
+  
+  $scope.$watch('search', function(newValue, oldValue) {
+    if (newValue == undefined || newValue == '') {
+      $scope.comboBoxOpen = false;
+    } else {$scope.comboBoxOpen = true}
+  });
+  
+  $scope.toggleComboBox = function() {
+    $scope.comboBoxOpen = !$scope.comboBoxOpen;
+  };
   
   $scope.loadTags = function(tags) {
-    _.each(tags, function(tag){  $scope.tags.push(tag)  });
+    $scope.tags = tags;
+  };
+  
+  $scope.select = function(tag) {
+    tag.selected = true;
+    $scope.search = '';
   };
 };
