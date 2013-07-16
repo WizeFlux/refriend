d235fdf0d83c54e281eb6b15b061bf383ea85fc7
W!ze Flux <fear.ru@gmail.com>
2013-07-17 05:38:04 +0800

    better page resize

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -27,6 +27,7 @@ Static = function($scope) {
         VK.api("users.get", {uids: $scope.people_uids, fields: "uid, first_name, last_name, nickname, photo, photo_medium, photo_big"}, function(data) {
           $scope.$apply(function(){  $scope.people = data.response; $scope.people_loading = false  })
         });
+        $scope.adjustIframeHeight();
       }, 100);      
     } else {  $scope.people_require_reload = true  }    
   };
@@ -37,6 +38,7 @@ Static = function($scope) {
       setTimeout(function(){  
         VK.api("places.getCityById", {cids: $scope.cities_cids}, function(data) {
           $scope.$apply(function(){  $scope.cities = data.response; $scope.cities_loading = false  })
+          $scope.adjustIframeHeight();
         });
       }, 100);
     } else {  $scope.cities_require_reload = true  } 
@@ -44,10 +46,11 @@ Static = function($scope) {
 
 
   $scope.docHeight = function() {  return document.body.offsetHeight  };
+  $scope.adjustIframeHeight = function() {  VK.callMethod("resizeWindow", 800, $scope.docHeight())  }
   $scope.people_uids_size = function() {  return $scope.people_uids.length  };
   $scope.cities_cids_size = function() {  return $scope.cities_cids.length  };
   
   $scope.$watch('people_uids_size()', function(nV, oV) {if(nV > 0){  $scope.load_people()  }});
   $scope.$watch('cities_cids_size()', function(nV, oV) {if(nV > 0){  $scope.load_cities()  }});
-  $scope.$watch('docHeight()', function(nV, oV) {  VK.callMethod("resizeWindow", 800, nV)  });
+  $scope.$watch('docHeight()', function(nV, oV) {  $scope.adjustIframeHeight()  });
 };
